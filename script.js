749bac9bea10a745355709a882264bda73296ec1
Dnyaneshwar Magar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2024-04-14 11:24:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+document.getElementById("submit").addEventListener("click",(e)=>{
+	e.preventDefault();
+	let username = document.getElementById("username").value;
+	let password = document.getElementById("password").value;
+	var rememberMe = document.getElementById('checkbox').checked;
+
+        if (rememberMe) {
+            localStorage.setItem('username', username);
+            localStorage.setItem('password', password);
+        } else {
+            localStorage.removeItem('username');
+            localStorage.removeItem('password');
+        }
+
+        alert('Logged in as ' + username);	
+});
+if (localStorage.getItem('username') && localStorage.getItem('password')) {
+        var existingButton = document.createElement('button');
+        existingButton.textContent = 'Login as existing user';
+        existingButton.id = 'existing';
+        existingButton.addEventListener('click', function() {
+            var username = localStorage.getItem('username');
+            alert('Logged in as ' + username);
+        });
+        document.body.appendChild(existingButton);
+    }
